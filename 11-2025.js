ce0eaea5dccfc65e30ac50cca04766cc8e93809c
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-11-05 07:58:23 +0100

    Update 11-2025.js

--- 11-2025.js
+++ 11-2025.js
@@ -83,3 +83,18 @@ function imageSearch(images, term) {
   }
   return res;
 }
+
+/* 05-11-2025: Matrix Builder
+Given two integers (a number of rows and a number of columns), return a matrix (an array of arrays) filled with zeros (0) of the given size.
+
+For example, given 2 and 3, return:
+
+[
+  [0, 0, 0],
+  [0, 0, 0]
+]
+*/
+
+function buildMatrix(rows, cols) {
+  return Array(rows).fill("").map((item)=>Array(cols).fill(0));
+}
