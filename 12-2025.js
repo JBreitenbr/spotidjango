d7f03fdb3c7121ec7ce0d0c70043520ba56ee92f
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-12-02 07:16:26 +0100

    Update 12-2025.js

--- 12-2025.js
+++ 12-2025.js
@@ -8,3 +8,30 @@ Round the result to two decimal places. */
 function convertToKm(miles) {
   return Math.round(miles*1.60934*100)/100;
 }
+
+/* Camel to Snake
+Given a string in camel case, return the snake case version of the string using the following rules:
+
+The input string will contain only letters (A-Z and a-z) and will always start with a lowercase letter.
+Every uppercase letter in the camel case string starts a new word.
+Convert all letters to lowercase.
+Separate words with an underscore (_). */
+
+function toSnake(camel) {
+  let a="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  let l=camel.length;
+  let c=camel.split("");
+  let ind=[0];
+  let res=[];
+  for(let i=0;i<camel.length;i++){
+     if(a.includes(camel[i])){
+       ind.push(i);
+     }
+  }
+  ind.push(l);
+  for(let i=1;i<ind.length;i++){
+    res.push(camel.slice(ind[i-1],ind[i]));
+  }
+  let s=res.map((item)=>item.toLowerCase()).join("_");
+  return s;
+}
