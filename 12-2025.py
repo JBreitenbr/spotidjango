14ee54aabf9870406ed4c234161b3d348e69edc0
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-12-13 11:41:03 +0100

    Update 12-2025.py

--- 12-2025.py
+++ 12-2025.py
@@ -267,3 +267,50 @@ def update_inventory(inv, ship):
         res.append([d[k],k])
     return res
 
+""" 13-12-2025: Game of Life
+Given a matrix (array of arrays) representing the current state in Conway's Game of Life, return the next state of the matrix using these rules:
+
+Each cell is either 1 (alive) or 0 (dead).
+A cell's neighbors are the up to eight surrounding cells (vertically, horizontally, and diagonally).
+Cells on the edges have fewer than eight neighbors.
+Rules for updating each cell:
+
+Any live cell with fewer than two live neighbors dies (underpopulation).
+Any live cell with two or three live neighbors lives on.
+Any live cell with more than three live neighbors dies (overpopulation).
+Any dead cell with exactly three live neighbors becomes alive (reproduction).
+For example, given:
+
+[
+  [0, 1, 0],
+  [0, 1, 1],
+  [1, 1, 0]
+]
+return:
+
+[
+  [0, 1, 1],
+  [0, 0, 1],
+  [1, 1, 1]
+]
+Each cell updates according to the number of live neighbors. 
+For instance, [0][0] stays dead (2 live neighbors), [0][1] stays alive (2 live neighbors), [0][2] dies (3 live neighbors), and so on. """
+
+def game_of_life(grid):
+    m,n = len(grid),len(grid[0])
+    new = [[0 for _ in range(n)] for _ in range(m)]
+    dirs = [(0, 1),(1, 0),(0, -1),(-1, 0),(1, 1),(-1, -1),(1, -1),(-1, 1)]
+    for i in range(m):
+        for j in range(n):
+            live=0
+            for dx,dy in dirs:
+                x,y=i+dx,j+dy
+                if 0 <= x < m and 0 <= y < n and (grid[x][y] == 1):
+                    live+=1
+            if grid[i][j] == 1 and (live<2 or live>3):
+                new[i][j]=0
+            elif grid[i][j]==0 and live==3:
+                new[i][j]=1
+            else:
+                new[i][j]=grid[i][j]
+    return new
