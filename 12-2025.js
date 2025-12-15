f2cc4bc59c9ce389a91f555684947557cf6478de
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-12-15 07:55:51 +0100

    Update 12-2025.js

--- 12-2025.js
+++ 12-2025.js
@@ -362,3 +362,16 @@ function titleCase(title) {
   let sp=title.split(" ").map((item)=>item[0].toUpperCase()+item.slice(1).toLowerCase());
   return sp.join(" ");
 }
+
+/* 15-12-2025: Speed Check
+Given the speed you are traveling in miles per hour (MPH), and a speed limit in kilometers per hour (KPH), determine whether you are speeding and if you will get a warning or a ticket.
+
+1 mile equals 1.60934 kilometers.
+If you are travelling less than or equal to the speed limit, return "Not Speeding".
+If you are travelling 5 KPH or less over the speed limit, return "Warning".
+If you are travelling more than 5 KPH over the speed limit, return "Ticket".*/
+
+function speedCheck(speedMph, speedLimitKph) {
+  let speedKph=1.60934*speedMph
+  return speedKph<=speedLimitKph?"Not Speeding":speedKph-5<=speedLimitKph?"Warning":"Ticket";
+}
