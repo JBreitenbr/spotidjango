0fd176dcd6868e6012965d7cb0453cbfa81fb1bf
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-12-17 07:19:30 +0100

    Update 12-2025.py

--- 12-2025.py
+++ 12-2025.py
@@ -359,3 +359,24 @@ def has_consonant_count(text, target):
         if ch.lower() in c:
             sn+=1
     return sn==target
+    
+""" 17-12-2025: Markdown Blockquote Parser
+Given a string that includes a blockquote in Markdown, return the equivalent HTML string.
+
+A blockquote in Markdown is any line that:
+
+Starts with zero or more spaces
+Followed by a greater-than sign (>)
+Then, one or more spaces
+And finally, the blockquote text.
+Return the blockquote text surrounded by opening and closing HTML blockquote tags.
+
+For example, given "> This is a quote", return <blockquote>This is a quote</blockquote>.
+
+Note: The console may not display HTML tags in strings when logging messages. Check the browser console to see logs with tags included."""
+
+def parse_blockquote(markdown):
+    md=markdown.strip()
+    sp=" ".join(md.split(" ")[1:]).strip()
+    stri="<blockquote>"+sp+"</blockquote>"
+    return stri
