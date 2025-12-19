0ee853d739eb62a9bd3f4779fd2df6edec98bcf0
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-12-19 07:11:34 +0100

    Add pairwise function for finding index pairs
    Implement pairwise function to find indices of pairs that sum to target.

--- 12-2025.js
+++ 12-2025.js
@@ -447,3 +447,26 @@ function createBoard(dims) {
   }
   return res;
 }
+
+/* 19-12-2025: Pairwise
+Given an array of integers and a target number, find all pairs of elements in the array whose values add up to the target and return the sum of their indices.
+
+For example, given [2, 3, 4, 6, 8] and 10, you will find two valid pairs:
+
+2 and 8 (2 + 8 = 10), whose indices are 0 and 4
+4 and 6 (4 + 6 = 10), whose indices are 2 and 3
+Add all the indices together to get a return value of 9.*/
+
+function pairwise(arr, target) {
+  let p=[];
+  for(let i=0;i<arr.length;i++){
+    for(let j=i;j<arr.length;j++){
+      if(arr[i]+arr[j]==target && i!=j){
+        p.push(i);
+        p.push(j);
+      }
+    }
+  }
+  let res=p.reduce((a,b)=>a+b,0);
+  return res;
+}
