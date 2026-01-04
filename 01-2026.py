26d17c1fcc50b0c013e4cba5055dde8c5a8ff4a0
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-01-04 11:35:39 +0100

    Update 01-2026.py

--- 01-2026.py
+++ 01-2026.py
@@ -41,3 +41,54 @@ def nth_fibonacci(n):
     for i in range(4,n+1):
         dp[i]=dp[i-1]+dp[i-2]
     return dp[n]
+    
+""" 03-01-2026: Left-Handed Seat at the Table
+Given a 4x2 matrix (array of arrays) representing the seating arrangement for a meal, determine how many seats a left-handed person can sit at.
+
+A left-handed person cannot sit where a right-handed person would be in the seat to the immediate left of them.
+In the given matrix:
+
+An "R" is a seat occupied by a right-handed person.
+An "L" is a seat occupied by a left-handed person.
+An "U" is an unoccupied seat.
+Only unoccupied seats are available to sit at.
+The seats in the top row are facing "down", and the seats in the bottom row are facing "up" (like a table), so left and right are relative to the seat's orientation.
+Corner seats only have one seat next to them.
+For example, in the given matrix:
+
+[
+  ["U", "R", "U", "L"],
+  ["U", "R", "R", "R"]
+]
+The top-left seat is cannot be sat in because there's a right-handed person to the left. The other two open seats can be sat in because there isn't a right-handed person to the left.
+"""
+
+def find_left_handed_seats(table):
+    row1=table[0]
+    row2=table[1]
+    rev=list("".join(table[1])[::-1])
+    sn=0
+    for i in range(len(row1)-1):
+        if row1[i]=="U" and row1[i+1]!="R":
+            sn+=1
+        if rev[i]=="U" and rev[i+1]!="R":
+            sn+=1
+    if row1[-1]=="U":
+        sn+=1
+    if rev[-1]=="U":
+        sn+=1
+    return sn
+
+""" 04-01-2026: Leap Year Calculator
+Given an integer year, determine whether it is a leap year.
+
+A year is a leap year if it satisfies the following rules:
+
+The year is evenly divisible by 4, and
+The year is not evenly divisible by 100, unless
+The year is evenly divisible by 400. 
+"""
+
+def is_leap_year(year):
+    return year%400==0 or year%4==0 and year%100!=0
+
