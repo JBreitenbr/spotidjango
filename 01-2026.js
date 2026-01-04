a20b8f2a8ea33b8f67613a21d11a4471bb5bba97
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-01-04 11:32:15 +0100

    Update 01-2026.js

--- 01-2026.js
+++ 01-2026.js
@@ -81,4 +81,17 @@ function findLeftHandedSeats(table) {
     }
   }
   return sn;
-  }
+}
+
+/* 04-01-2026: Leap Year Calculator
+Given an integer year, determine whether it is a leap year.
+
+A year is a leap year if it satisfies the following rules:
+
+The year is evenly divisible by 4, and
+The year is not evenly divisible by 100, unless
+The year is evenly divisible by 400. */
+
+function isLeapYear(year) {
+  return year%400==0||year%4==0&&year%100!=0;
+}
